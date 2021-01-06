9e67a1fd5b235bb96c54210b72a8df7e6cdf64d8
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-01-06 22:14:38 +0300

    Create FooterVM.cs

---
+++ HB.Presentation/Models/ViewComponent/FooterVM.cs
@@ -0,0 +1,17 @@
+﻿using HB.Presentation.Models.Reservation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HB.Presentation.Models.ViewComponent
+{
+    public class FooterVM
+    {
+        public FooterVM()
+        {
+            Item = new List<ReservationMM>();
+        }
+        public List<ReservationMM> Item { get; set; }
+    }
+}
