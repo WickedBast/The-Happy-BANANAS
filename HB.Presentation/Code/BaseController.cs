c65e119cd923f65db92234dc2ea3b55b24c161bc
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-01-08 22:09:20 +0300

    Create BaseController.cs

---
+++ HB.Presentation/Code/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HB.Presentation.Code
+{
+    public class BaseController : Controller
+    {
+        public Guid CurrentUserID
+        {
+            get
+            {
+                return Guid.Parse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == "Id").Value);
+            }
+        }
+
+        public string CurrentUserName
+        {
+            get
+            {
+                return HttpContext.User.Claims.FirstOrDefault(x => x.Type == "Name").Value;
+            }
+        }
+        public string CurrentUserLastName
+        {
+            get
+            {
+                return HttpContext.User.Claims.FirstOrDefault(x => x.Type == "Surname").Value;
+            }
+        }
+        public string CurrentUserEmail
+        {
+            get
+            {
+                return HttpContext.User.Claims.FirstOrDefault(x => x.Type == "Email").Value;
+            }
+        }
+    }
+}
