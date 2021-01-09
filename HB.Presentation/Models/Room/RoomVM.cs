b829c3fb952a59b9ac4c691e38af14e73238c415
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-01-10 02:05:48 +0300

    Create RoomVM.cs

---
+++ HB.Presentation/Models/Room/RoomVM.cs
@@ -0,0 +1,44 @@
+﻿using HB.Entity.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HB.Presentation.Models.Room
+{
+    public class RoomVM
+    {
+        public RoomVM()
+        {
+            Item = new List<RoomMM>();
+        }
+
+        public List<RoomMM> Item { get; set; }
+        public int TotalCount { get; set; }
+        public int PageCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+
+    public class RoomMM
+    {
+        public RoomMM()
+        {
+            User = new UserMM();
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }        
+        public UserMM User { get; set; }
+        public int PersonCapacity { get; set; }
+        public string Type { get; set; }
+        public RoomImage RoomImage { get; set; }
+
+    }
+
+    public class UserMM
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
