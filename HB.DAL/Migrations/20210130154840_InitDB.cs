ba8e3f4b8d15a132fa0925ff94639524770980b6
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-01-30 18:49:49 +0300

    Create 20210130154840_InitDB.cs

---
+++ HB.DAL/Migrations/20210130154840_InitDB.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HB.DAL.Migrations
+{
+    public partial class InitDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    RoomNumber = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    IsFull = table.Column<bool>(nullable: false),
+                    PersonCapacity = table.Column<int>(nullable: false),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Role = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomImage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    RoomID = table.Column<Guid>(nullable: false),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomImage_Room_RoomID",
+                        column: x => x.RoomID,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExtraService",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    UserID = table.Column<Guid>(nullable: true),
+                    ServiceType = table.Column<string>(nullable: true),
+                    PNRNumber = table.Column<string>(nullable: true),
+                    Cost = table.Column<decimal>(nullable: false),
+                    Quota = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Hour = table.Column<string>(nullable: true),
+                    NumberOfPerson = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExtraService", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExtraService_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    UserID = table.Column<Guid>(nullable: true),
+                    Slug = table.Column<string>(nullable: true),
+                    RateAVG = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    Cost = table.Column<decimal>(nullable: false),
+                    RoomNumber = table.Column<Guid>(nullable: false),
+                    NumberOfPerson = table.Column<int>(nullable: false),
+                    RoomCount = table.Column<int>(nullable: false),
+                    PNRNumber = table.Column<string>(nullable: true),
+                    RoomId = table.Column<Guid>(nullable: true),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    Night = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reservation_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    ReservationID = table.Column<Guid>(nullable: false),
+                    CommentText = table.Column<string>(nullable: true),
+                    UserID = table.Column<Guid>(nullable: true),
+                    RoomNumber = table.Column<Guid>(nullable: false),
+                    FullName = table.Column<string>(nullable: true),
+                    RateGiven = table.Column<decimal>(type: "decimal(18,1)", nullable: false),
+                    RoomId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comment_Reservation_ReservationID",
+                        column: x => x.ReservationID,
+                        principalTable: "Reservation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comment_Room_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Room",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comment_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ReservationID",
+                table: "Comment",
+                column: "ReservationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_RoomId",
+                table: "Comment",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_UserID",
+                table: "Comment",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExtraService_UserID",
+                table: "ExtraService",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_RoomId",
+                table: "Reservation",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_UserID",
+                table: "Reservation",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomImage_RoomID",
+                table: "RoomImage",
+                column: "RoomID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "ExtraService");
+
+            migrationBuilder.DropTable(
+                name: "RoomImage");
+
+            migrationBuilder.DropTable(
+                name: "Reservation");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
