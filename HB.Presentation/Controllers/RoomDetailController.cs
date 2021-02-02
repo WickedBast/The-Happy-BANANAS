b7d59d8aad20095247003cdfab081ad9aaa88afc
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-02 15:06:43 +0300

    Update RoomDetailController.cs

--- HB.Presentation/Controllers/RoomDetailController.cs
+++ HB.Presentation/Controllers/RoomDetailController.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HB.Presentation.Models.Room;
+using HB.Repository.Interface.Application;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HB.Presentation.Controllers
 {
 	public class RoomDetailController : Controller
 	{
+		public readonly IRoomRepository roomRepo;
+		public readonly IRoomImageRepository roomImageRepo;
+
+		public RoomDetailController(
+			IRoomRepository roomRepo,
+			IRoomImageRepository roomImageRepo)
+        {
+			this.roomRepo = roomRepo;
+			this.roomImageRepo = roomImageRepo;
+        }
 		public IActionResult Index()
 		{
-			return View();
+			var result = new RoomVM();
+
+			var query = roomRepo.GetBy(x => true);
+
+			result.Item = query.OrderByDescending(x => x.CreateDate).Take(10).Select(x => new RoomMM 
+			{
+				Id = x.Id,
+				CreateDate = x.CreateDate,
+				PersonCapacity = x.PersonCapacity,
+				Cost = x.Price,
+				Type = x.Type,
+
+			}).ToList();
+
+			return View(result);
 		}
 	}
 }
