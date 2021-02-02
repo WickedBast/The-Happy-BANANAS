7b1e8c152135be0f7f40665f58ccc42d8e55847a
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-02 10:51:41 +0300

    Create 20210202074934_Changes.cs

---
+++ HB.DAL/Migrations/20210202074934_Changes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HB.DAL.Migrations
+{
+    public partial class Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "Comment",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "Comment",
+                type: "char(36)",
+                nullable: true,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
