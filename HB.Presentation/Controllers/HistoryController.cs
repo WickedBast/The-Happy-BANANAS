0402b11c0fd680771544c58165b2bc1fe5690d54
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-02 15:06:41 +0300

    Update HistoryController.cs

--- HB.Presentation/Controllers/HistoryController.cs
+++ HB.Presentation/Controllers/HistoryController.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HB.Presentation.Models.Reservation;
+using HB.Repository.Interface.Application;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HB.Presentation.Controllers
 {
 	public class HistoryController : Controller
 	{
+		public readonly IReservationRepository reservationRepo;
+		public readonly IRoomImageRepository imageRepo;
+
+		public HistoryController(
+			IReservationRepository reservationRepo,
+			IRoomImageRepository imageRepo)
+        {
+			this.reservationRepo = reservationRepo;
+			this.imageRepo = imageRepo;
+        }
 		public IActionResult Index()
 		{
-			return View();
+			var result = new ReservationVM();
+
+			var query = reservationRepo.GetBy(x => true);
+
+			result.Items = query.OrderByDescending(x => x.CreateDate).Take(10).Select(x => new ReservationMM
+			{
+				Id = x.Id,
+				Cost = x.Cost,
+				RoomCount = x.RoomCount,
+				StartDate = x.StartDate,
+				NumberOfPerson = x.NumberOfPerson,
+				Night = x.Night,
+				Type = x.Room.Type,
+				CreateDate = x.CreateDate.Value,
+
+			}).ToList();
+
+			return View(result);
 		}
 	}
 }
