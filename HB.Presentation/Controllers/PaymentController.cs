4f87b09715266d507e3d9ffb9aec967b21533ace
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-03 20:46:57 +0300

    Update PaymentController.cs

--- HB.Presentation/Controllers/PaymentController.cs
+++ HB.Presentation/Controllers/PaymentController.cs
@@ -36,78 +36,78 @@ namespace HB.Presentation.Controllers
 			return View();
 		}
 
-		public async Task<IActionResult> Payment(IFormCollection frm)
-		{
-			var fullname = frm["txtFullName"];
-			var email = frm["txtEmail"].ToString();
-			var address = frm["txtAddress"];
-			var city = frm["txtCity"];
-			var zip = frm["txtZip"];
-			var state = frm["txtState"];
+        public IActionResult Payment(IFormCollection frm)
+        {
+            var fullname = frm["txtFullName"];
+            var email = frm["txtEmail"].ToString();
+            var address = frm["txtAddress"];
+            var city = frm["txtCity"];
+            var zip = frm["txtZip"];
+            var state = frm["txtState"];
 
-			var name = frm["txtCardname"];
-			var cardNo = frm["txtCardnumber"];
-			var expMonth = frm["txtExpmonth"];
-			var cvv = frm["txtCVV"];
-			var expYear = frm["txtExpyear"];
-			var expYear2 = Int32.Parse(expYear);
+            var name = frm["txtCardname"];
+            var cardNo = frm["txtCardnumber"];
+            var expMonth = frm["txtExpmonth"];
+            var cvv = frm["txtCVV"];
+            var expYear = frm["txtExpyear"];
+            var expYear2 = Int32.Parse(expYear);
 
-			var reservation = reservationRepo.FirstOrDefaultBy(x => x.Id == CurrentUserID);
-			var total = reservation.Room.Price;
+            var reservation = reservationRepo.FirstOrDefaultBy(x => x.Id == CurrentUserID);
+            var total = reservation.Room.Price;
 
-			if (
-				string.IsNullOrWhiteSpace(fullname) ||
-				string.IsNullOrWhiteSpace(email) ||
-				string.IsNullOrWhiteSpace(address) ||
-				string.IsNullOrWhiteSpace(city) ||
-				string.IsNullOrWhiteSpace(zip) ||
-				string.IsNullOrWhiteSpace(state) ||
-				string.IsNullOrWhiteSpace(name) ||
-				string.IsNullOrWhiteSpace(cardNo) ||
-				string.IsNullOrWhiteSpace(expMonth) ||
-				string.IsNullOrWhiteSpace(cvv) ||
-				string.IsNullOrWhiteSpace(expYear) 
-				)
-			{
-				TempData["Info"] = "Lütfen bütün alanları doldurun.";
-				return RedirectToAction("Index", "Payment");
-			}
-			else if (!email.ToString().IsValidEmailAddress())
-			{
-				TempData["Info"] = "Lütfen geçerli bir e-posta adresi girin.";
-				return RedirectToAction("Index", "Payment");
-			}
-			else if (expYear2 <= 2021)
-			{
-				TempData["Info"] = "Kartınızın son geçerlilik tarihi dolmuştur.";
-				return RedirectToAction("Index", "Payment");
-			}
-			else
-			{
-				//var PNRCode = new Cryptography().GenerateKey(6, true);
-				var PNRCode = reservation.PNRNumber;
-				var user = userRepo.FirstOrDefaultBy(x => x.Id == CurrentUserID);
-				reservation.IsPaid = true;
+            if (
+                string.IsNullOrWhiteSpace(fullname) ||
+                string.IsNullOrWhiteSpace(email) ||
+                string.IsNullOrWhiteSpace(address) ||
+                string.IsNullOrWhiteSpace(city) ||
+                string.IsNullOrWhiteSpace(zip) ||
+                string.IsNullOrWhiteSpace(state) ||
+                string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(cardNo) ||
+                string.IsNullOrWhiteSpace(expMonth) ||
+                string.IsNullOrWhiteSpace(cvv) ||
+                string.IsNullOrWhiteSpace(expYear)
+                )
+            {
+                TempData["Info"] = "Lütfen bütün alanları doldurun.";
+                return RedirectToAction("Index", "Payment");
+            }
+            else if (!email.ToString().IsValidEmailAddress())
+            {
+                TempData["Info"] = "Lütfen geçerli bir e-posta adresi girin.";
+                return RedirectToAction("Index", "Payment");
+            }
+            else if (expYear2 <= 2021)
+            {
+                TempData["Info"] = "Kartınızın son geçerlilik tarihi dolmuştur.";
+                return RedirectToAction("Index", "Payment");
+            }
+            else
+            {
+                //var PNRCode = new Cryptography().GenerateKey(6, true);
+                var PNRCode = reservation.PNRNumber;
+                var user = userRepo.FirstOrDefaultBy(x => x.Id == CurrentUserID);
+                reservation.IsPaid = true;
 
-				var message = new MimeMessage();
-				message.From.Add(new MailboxAddress("PNRNumber", "alpnce@gmail.com"));
-				message.To.Add(new MailboxAddress(user.Name, user.Email));
-				message.Subject = "PNR Number:";
-				message.Body = new TextPart("plain")
-				{
-					Text = "Rezervasyonunuz alınmıştır. Rezervasyonunuz için PNR Numaranız:  " + reservation.PNRNumber
-				};
-				using (var client = new SmtpClient())
-				{
-					client.Connect("smtp.gmail.com", 587, false);
-					client.Authenticate("hotelstar43@gmail.com", "happyBananas");
-					client.Send(message);
-					client.Disconnect(true);
-				}
-			}
-			TempData["Info"] = "Satın alım işleminiz gerçekleştirilmiştir.";
-			return RedirectToAction("Index", "Home");
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("PNRNumber", "alpnce@gmail.com"));
+                message.To.Add(new MailboxAddress(user.Name, user.Email));
+                message.Subject = "PNR Number:";
+                message.Body = new TextPart("plain")
+                {
+                    Text = "Rezervasyonunuz alınmıştır. Rezervasyonunuz için PNR Numaranız:  " + reservation.PNRNumber
+                };
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("hotelstar43@gmail.com", "happyBananas");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+            }
+            TempData["Info"] = "Satın alım işleminiz gerçekleştirilmiştir.";
+            return RedirectToAction("Index", "Home");
 
-		}
-	}
+        }
+    }
 }
