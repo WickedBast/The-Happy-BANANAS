dac02f43b299be3bb111dfa2195967794ba4477a
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-03 20:47:02 +0300

    Update RoomAddController.cs

--- HB.Presentation/Controllers/RoomAddController.cs
+++ HB.Presentation/Controllers/RoomAddController.cs
@@ -1,8 +1,11 @@
-﻿using HB.Repository.Interface.Application;
+﻿using HB.Core.Extensions;
+using HB.Entity.Application;
+using HB.Repository.Interface.Application;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,14 +28,41 @@ namespace HB.Presentation.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(IFormCollection frm)
+        public async Task<IActionResult> AddAsync(IFormCollection frm, List<IFormFile> Images)
         {
+            var RoomName = frm["txtRoomName"];
             var RoomType = frm["txtRoomType"];
             var RoomNumber = frm["txtRoomNumber"];
             var MaxPerson = frm["txtPersonCapacity"];
             var Price = frm["txtPrice"];
 
+            var ImageList = new List<RoomImage>();
+
+            if (Images != null && Images.Any())
+            {
+                foreach (var Image in Images)
+                {
+                    var fileName = Path.GetFileName(Image.FileName);
+                    string Extension = Path.GetExtension(fileName).ToLower();
+                    string NewFileName = Guid.NewGuid().ToString();
+                    if (Extension == ".jpg" || Extension == ".png" || Extension == ".jfif")
+                    {
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", NewFileName + Extension);
+                        using (var fileSteam = new FileStream(filePath, FileMode.Create))
+                        {
+                            await Image.CopyToAsync(fileSteam);
+                        }
+
+                        ImageList.Add(new RoomImage
+                        {
+                            Image = NewFileName + Extension,
+                        });
+                    }
+                }
+            }
+
             if (
+               string.IsNullOrWhiteSpace(RoomName)||
                string.IsNullOrWhiteSpace(RoomType) ||
                string.IsNullOrWhiteSpace(RoomNumber) ||
                string.IsNullOrWhiteSpace(MaxPerson) ||
@@ -45,14 +75,17 @@ namespace HB.Presentation.Controllers
             //else if (RoomNumber)
             //{
 
-            //}
+            //}           
 
-            roomRepo.Add(new Entity.Application.Room
+            roomRepo.Add(new Room
             {
+                Name = RoomName.ToString(),
                 Type = RoomType.ToString(),
                 RoomNumber = RoomNumber.ToString(),
-                PersonCapacity = Int32.Parse(MaxPerson) ,
-                Price = Int32.Parse(Price)
+                PersonCapacity = Int32.Parse(MaxPerson),
+                Price = Int32.Parse(Price),
+                Slug = RoomName.ToString().ToUrlSlug(),
+                RoomImages = ImageList
             });
 
             TempData["Info"] = "Oda kayıt işleminiz gerçekleştirilmiştir.";
