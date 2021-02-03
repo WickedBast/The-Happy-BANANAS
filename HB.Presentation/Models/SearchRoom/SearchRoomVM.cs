655c194241ca8edf17bb430a212ef6468be7383d
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-03 20:47:08 +0300

    Create SearchRoomVM.cs

---
+++ HB.Presentation/Models/SearchRoom/SearchRoomVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HB.Presentation.Models.SearchRoom
+{
+    public class SearchRoomVM
+    {
+        public DateTime? DateFrom { get; set; } = null;
+        public DateTime? DateTo { get; set; } = null;
+        public int NoOfRooms { get; set; }
+        public int NoOfPerson { get; set; }
+        public string Type { get; set; }
+        //public IList<Room> Room { get; set; } = new List<Room>();
+    }
+}
