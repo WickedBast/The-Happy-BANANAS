81579e2a5da6752f20d984add8dc7f85624791d3
Oğul Tütüncü <ogul.tutuncu@isik.edu.tr>
2021-02-04 03:32:07 +0300

    Create 20210204002229_Update.cs

---
+++ HB.DAL/Migrations/20210204002229_Update.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HB.DAL.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Cost",
+                table: "ExtraService",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cost",
+                table: "ExtraService",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
